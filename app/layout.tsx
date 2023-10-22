62c74741a41aefc523205a78e041e89117c36748
lyg509 <dydrjs252@naver.com>
2023-10-22 21:10:44 +0900

    feat(CustomFonts): add custom fonts functionality to app

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const metadata: Metadata = {
+  title: "Ecommerce",
+  description: "Ecommerce app",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
