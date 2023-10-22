e85506c6b11e8ed2582b4df83def3ab49529e27a
lyg509 <dydrjs252@naver.com>
2023-10-22 21:41:58 +0900

    feat: add app layouts functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/nav/NavBar";
+import Footer from "./components/footer/Footer";
+
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -12,7 +15,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={`${poppins.className} text-slate-700`}>
+        <div className="flex flex-col min-h-screen">
+          <NavBar />
+          <main className="flex-grow bg-black">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main>Ecommerce</main>;
+  return <div>Ecommerce</div>;
 }
