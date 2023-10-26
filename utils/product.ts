0500e7b6ace54e60ce45de59734ac7cb65f20e1c
lyg509 <dydrjs252@naver.com>
2023-10-26 18:10:47 +0900

    feat(Utils): Adds Sample Format to the utils

---
+++ utils/product.ts
@@ -0,0 +1,24 @@
+export const product = {
+  id: "64a654593e91b8e73a351e9b",
+  name: "iphone 14",
+  description: "Short description",
+  price: 2999,
+  brand: "apple",
+  category: "Phone",
+  inStock: true,
+  images: [
+    {
+      color: "White",
+      colorCode: "#FFFFFF",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/ecommerce-shop-cc542.appspot.com/o/products%2F1688622161445-iphone14-white.png?alt=media&token=fe2065e5-fdfe-4a6f-baa6-380b5fad90b8",
+    },
+    {
+      color: "Gray",
+      colorCode: "#808080",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/ecommerce-shop-cc542.appspot.com/o/products%2F1688622165836-iphone14-gray.png?alt=media&token=58f684db-998e-43eb-aa06-efe3d6ccfad4",
+    },
+  ],
+  reviews: [],
+};
