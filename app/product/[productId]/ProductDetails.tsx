09112f8b87903e494ff6245352d5dd09d2a01bb5
lyg509 <dydrjs252@naver.com>
2023-10-28 16:54:37 +0900

    feat(ButtonComponent): Add a reusable Button component

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,11 +1,27 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import SetColor from "@/app/components/products/SetColor";
+import SetQuatity from "@/app/components/products/SetQuatity";
 import { Rating } from "@mui/material";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 interface ProductDetailProps {
   product: any;
 }
 
 export type CartProductType = {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  brand: string;
+  selectedImg: SelectedImgType;
+  quantity: number;
+  price: number;
+};
+
+export type CartProduct = {
   id: string;
   name: string;
   description: string;
@@ -40,10 +56,39 @@ const ProductDetails: React.FC<ProductDetailProps> = ({ product }) => {
 
   const productRating = product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) / product.reviews.length;
 
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [cartProduct.selectedImg]
+  );
+
+  const handleQtyIncrease = useCallback(() => {
+    if (cartProduct.quantity === 99) {
+      return;
+    }
+
+    setCartProduct((prev) => {
+      return { ...prev, quantity: ++prev.quantity };
+    });
+  }, [cartProduct]);
+
+  const handleQtyDecrease = useCallback(() => {
+    if (cartProduct.quantity === 1) {
+      return;
+    }
+
+    setCartProduct((prev) => {
+      return { ...prev, quantity: --prev.quantity };
+    });
+  }, [cartProduct]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
       <div>Images</div>
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1 text-slate-500">
         <h2
           className="text-3xl font-medium
       text-slate-700"
@@ -63,13 +108,15 @@ const ProductDetails: React.FC<ProductDetailProps> = ({ product }) => {
         <div>
           <span className="font-semibold">Brand:</span> {product.brand}
         </div>
-        <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>{product.InStock ? "In stock" : "Out of stock"}</div>
+        <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>{product.inStock ? "In stock" : "Out of stock"}</div>
         <Horizontal />
-        <div>color</div>
+        <SetColor cartProduct={cartProduct} images={product.images} handleColorSelect={handleColorSelect} />
         <Horizontal />
-        <div>qiatity</div>
+        <SetQuatity cartProduct={cartProduct} handleQtyIncrease={handleQtyIncrease} handleQtydecrease={handleQtyDecrease} />
         <Horizontal />
-        <div>add to cart</div>
+        <div className="max-w-[300px]">
+          <Button label="Add To Cart" onClinck={() => {}} />
+        </div>
       </div>
     </div>
   );
