9c8fde290ccf87c60980c13169beb9fb09609e85
lyg509 <dydrjs252@naver.com>
2023-11-02 18:13:10 +0900

    feat(CartClient, useCart): Add Remove Cart Item functionality to Cart List Components

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -6,6 +6,7 @@ type CartContextType = {
   cartTotalQty: number;
   cartProducts: CartProductType[] | null;
   handleAddProductToCart: (product: CartProductType) => void;
+  handleRemoveProductFromCart: (product: CartProductType) => void;
 };
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -24,6 +25,7 @@ export const CartContextProvider = (props: Props) => {
 
     setCartProducts(eProducts);
   }, []);
+
   const handleAddProductToCart = useCallback((product: CartProductType) => {
     setCartProducts((prev) => {
       let updatedCart;
@@ -40,10 +42,26 @@ export const CartContextProvider = (props: Props) => {
     });
   }, []);
 
+  const handleRemoveProductFromCart = useCallback(
+    (product: CartProductType) => {
+      if (cartProducts) {
+        const filteredProducts = cartProducts.filter((item) => {
+          return item.id === product.id;
+        });
+
+        setCartProducts(filteredProducts);
+        toast.success("Product removed");
+        localStorage.setItem("eCommerceCartItems", JSON.stringify(filteredProducts ));
+      }
+    },
+    [cartProducts]
+  );
+
   const value = {
     cartTotalQty,
     cartProducts,
     handleAddProductToCart,
+    handleRemoveProductFromCart,
   };
 
   return <CartContext.Provider value={value} {...props} />;
