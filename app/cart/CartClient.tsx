94006f431b16b0eeaeafd70eb4b122a82b7a4c97
lyg509 <dydrjs252@naver.com>
2023-11-02 23:29:59 +0900

    feat(UserCart, Product Page): Add Calculate SubTotal funtionality to UserCart and Product Page

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -8,7 +8,7 @@ import Button from "../components/Button";
 import ItemContent from "./ItemContent";
 
 const CartClient = () => {
-  const { cartProducts } = useCart();
+  const { cartProducts, handleClearCart } = useCart();
 
   console.log("CartProducts...", cartProducts);
 
@@ -52,7 +52,14 @@ const CartClient = () => {
       <div className="border-t-[1.5px] border-slate-200 py-4 flex justify-between gap-4">
         <div className="w-[90px">
           {" "}
-          <Button label="Clear Cart" onClick={() => {}} small outline />{" "}
+          <Button
+            label="Clear Cart"
+            onClick={() => {
+              handleClearCart();
+            }}
+            small
+            outline
+          />{" "}
         </div>
 
         <div className="text-sm flex flex-col gap-1 items-start">
--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -9,6 +9,7 @@ type CartContextType = {
   handleRemoveProductFromCart: (product: CartProductType) => void;
   handleCartQtyIncrease: (product: CartProductType) => void;
   handleCartQtyDecrease: (product: CartProductType) => void;
+  handleClearCart: () => void;
 };
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -18,8 +19,9 @@ interface Props {
 }
 
 export const CartContextProvider = (props: Props) => {
-  const [cartTotalQty, setCartTotalQty] = useState(100);
+  const [cartTotalQty, setCartTotalQty] = useState(0);
   const [cartProducts, setCartProducts] = useState<CartProductType[] | null>();
+  const [cartTotalAmount, setCartTotalAmout] = useState(0);
 
   useEffect(() => {
     const cartItems: any = localStorage.getItem("eCommerceCartItems");
@@ -27,6 +29,34 @@ export const CartContextProvider = (props: Props) => {
 
     setCartProducts(eProducts);
   }, []);
+
+  console.log("qty...", cartTotalQty);
+  console.log("amount...", cartTotalAmount);
+
+  useEffect(() => {
+    const getTotals = () => {
+      if (cartProducts) {
+        const { total, qty } = cartProducts?.reduce(
+          (acc, item) => {
+            const itemTotal = item.price * item.quantity;
+
+            acc.total += itemTotal;
+            acc.qty += item.quantity;
+
+            return acc;
+          },
+          {
+            total: 0,
+            qty: 0,
+          }
+        );
+
+        setCartTotalQty(qty);
+        setCartTotalAmout(total);
+      }
+    };
+    getTotals();
+  }, [cartProducts]);
 
   const handleAddProductToCart = useCallback((product: CartProductType) => {
     setCartProducts((prev) => {
@@ -104,6 +134,12 @@ export const CartContextProvider = (props: Props) => {
     },
     [cartProducts]
   );
+
+  const handleClearCart = useCallback(() => {
+    setCartProducts(null);
+    setCartTotalQty(0);
+    localStorage.setItem("eCommerceItems", JSON.stringify(null));
+  }, [cartProducts]);
 
   const value = {
     cartTotalQty,
