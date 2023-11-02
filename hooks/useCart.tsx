0cad24c6d36ebe43ccf4c7c05d46c6f810e5597f
lyg509 <dydrjs252@naver.com>
2023-11-02 23:56:40 +0900

    feat(NavBar, CartClient, UseCart): Add Display Cart Qty funtionality to NavBar and CartClient and UseCart

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -4,6 +4,7 @@ import { toast } from "react-hot-toast";
 
 type CartContextType = {
   cartTotalQty: number;
+  cartTotalAmount: number;
   cartProducts: CartProductType[] | null;
   handleAddProductToCart: (product: CartProductType) => void;
   handleRemoveProductFromCart: (product: CartProductType) => void;
@@ -143,6 +144,7 @@ export const CartContextProvider = (props: Props) => {
 
   const value = {
     cartTotalQty,
+    cartTotalAmount,
     cartProducts,
     handleAddProductToCart,
     handleRemoveProductFromCart,
